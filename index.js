50310d01ae9cb04fdc2a176ceaab59e5eeb92a88
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2021-11-08 21:10:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const wait = require('util').promisify(setTimeout);
+const express = require('express');
+const app = express();
+
+wait(1000)
+
+app.get('/', async ({ query }, response) => {
+ return response.sendFile('index/index.html', { root: '.' });
+});
+
+wait(1000)
+
+app.get('/add', async ({ query }, response) => {
+
+ const { que, ans, tipp } = query
+ const { clear } = query
+
+ if (que && ans && tipp) {
+  try {
+   const fs = require("fs")
+   let json = ""
+   json = require("./static/quiz.json")
+
+   var arr = [], length;
+   for (key in json) {
+    arr.push(key);
+   }
+   length = arr.length;
+
+   json[length + 1] = {
+    "frage": que,
+    "antwort": ans,
+    "tipp": tipp
+   }
+   fs.writeFileSync("./static/quiz.json", JSON.stringify(json))
+  } catch (error) {
+   console.error(error)
+  }
+ }
+
+ if (clear) {
+  try {
+   const fs = require("fs")
+   var json = require("./static/quiz.json")
+
+   json = {}
+   fs.writeFileSync("./static/quiz.json", JSON.stringify(json))
+  } catch (error) {
+   console.error(error)
+  }
+ }
+
+ return response.sendFile('index/add.html', { root: '.' });
+});
+
+wait(1000)
+
+app.get('/view', async ({ query }, response) => {
+ return response.sendFile('index/view.html', { root: '.' });
+});
+
+app.get('/edit', async ({ query }, response) => {
+
+ const { num, que, ans, tipp } = query
+
+ if (num && que && ans && tipp) {
+  try {
+   const fs = require("fs")
+   let json = ""
+   json = require("./static/quiz.json")
+
+   if(!json[num]) return
+
+   json[num] = {
+    "frage": que,
+    "antwort": ans,
+    "tipp": tipp
+   }
+   fs.writeFileSync("./static/quiz.json", JSON.stringify(json))
+  } catch (error) {
+   console.error(error)
+  }
+ }
+
+ return response.sendFile('index/edit.html', { root: '.' });
+});
+
+wait(1000)
+
+app.use("/static", express.static('./static/'));
+
+wait(1000)
+
+app.listen(80, function () {
+ console.log('Example app listening on port 80!');
+});
